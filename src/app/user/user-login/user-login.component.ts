197e97015561361b55aac2e7fe7614d30032969e
emiliaoana <emiliaoana01@gmail.com>
2024-03-05 17:35:09 +0200

    #9 resolved PR comments

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatButton} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatInput} from "@angular/material/input";
 import {NgIf} from "@angular/common";
-import {AuthenticationService} from "../services/AuthenticationService";
+import {AuthenticationService} from "../services/authentication.service";
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
@@ -32,10 +32,11 @@ export class UserLoginComponent {
 
   constructor(
     private authenticationService: AuthenticationService
-  ) { }
-  ngOnInit() { }
+  ) {
+  }
+
   signIn() {
-this.authenticationService.signIn(this.signInForm.value.username, this.signInForm.value.password);
+    this.authenticationService.signIn(this.signInForm.value.username, this.signInForm.value.password);
 
   }
 }
--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {NgIf} from "@angular/common";
 import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {Router, RouterLink, RouterLinkActive} from "@angular/router";
-import {AuthenticationService} from "../services/AuthenticationService";
+import {AuthenticationService} from "../services/authentication.service";
 import {MatIcon} from "@angular/material/icon";
 
 @Component({
@@ -34,10 +34,10 @@ export class UserRegisterComponent {
   });
 
   constructor(
-    private authenticationService: AuthenticationService,
-    private router: Router
-  ) { }
-  ngOnInit() { }
+    private authenticationService: AuthenticationService
+  ) {
+  }
+
   signUp() {
     this.authenticationService.register(this.signUpForm.value.username, this.signUpForm.value.password);
 
