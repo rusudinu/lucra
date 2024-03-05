43028da9dfbad388e492f47538bbc77f2356f38f
Rusu Dinu <dinu@codingshadows.com>
2024-03-05 20:44:20 +0200

    #1 user logout, displaying the user email

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
 import { MatButton } from '@angular/material/button';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [MatMenu, MatMenuItem, MatMenuTrigger, MatButton, MatToolbar, RouterLink, RouterLinkActive],
+    imports: [MatMenu, MatMenuItem, MatMenuTrigger, MatButton, MatToolbar, RouterLink, RouterLinkActive, NgbCollapse, NgIf],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    isNavbarCollapsed = true;
+
+    auth: Auth = inject(Auth);
+    router: Router = inject(Router);
+
+    logout() {
+        this.auth.signOut().then(() => {
+            this.router.navigate(['/sign-in']).then();
+        });
+    }
+
+    get loggedIn() {
+        return this.auth.currentUser !== null;
+    }
+
+    getUsername() {
+        return this.auth.currentUser?.email;
+    }
+}
