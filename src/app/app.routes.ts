436b75c77f3d6238eef396d3032df9b20608b41c
Rusu Dinu <dinu@codingshadows.com>
2024-03-05 19:48:57 +0200

    #1 added all app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,10 @@ export const routes: Routes = [
     { path: 'sign-in', component: UserLoginComponent },
     { path: 'sign-up', component: UserRegisterComponent },
     { path: 'dashboard', component: DashboardComponent },
+    { path: 'add-expense', component: DashboardComponent },
+    { path: 'add-income', component: DashboardComponent },
+    { path: 'investments-calculator', component: DashboardComponent },
+    { path: 'reports', component: DashboardComponent },
+    { path: 'pending-requests', component: DashboardComponent },
+    { path: 'previous-requests', component: DashboardComponent },
 ];
