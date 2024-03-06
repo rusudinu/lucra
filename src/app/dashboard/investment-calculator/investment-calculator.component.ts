d327db3bde1803002e9c9bb51a03d32e782c6349
Rusu Dinu <dinu@codingshadows.com>
2024-03-06 21:23:59 +0200

    #5 new chart library

--- src/app/dashboard/investment-calculator/investment-calculator.component.ts
+++ src/app/dashboard/investment-calculator/investment-calculator.component.ts
@@ -7,6 +7,7 @@ import { MatError, MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular
 import { MatInput } from '@angular/material/input';
 import { MatSelect } from '@angular/material/select';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
     selector: 'app-investment-calculator',
@@ -27,6 +28,7 @@ import { RouterLink, RouterOutlet } from '@angular/router';
         MatSuffix,
         CommonModule,
         RouterOutlet,
+        NgxChartsModule,
     ],
     templateUrl: './investment-calculator.component.html',
     styleUrl: './investment-calculator.component.scss',
@@ -42,9 +44,44 @@ export class InvestmentCalculatorComponent implements OnInit {
     frequencyOptions: string[] = ['Monthly', 'Annually'];
     computedValue: number = 0;
 
+    data: any[] = [
+        {
+            name: 'Income',
+            series: [],
+        },
+    ];
+    view: [number, number] = [700, 300];
+
+    // options
+    legend: boolean = true;
+    xAxis: boolean = true;
+    yAxis: boolean = true;
+    showYAxisLabel: boolean = true;
+    showXAxisLabel: boolean = true;
+    xAxisLabel: string = 'Year';
+    yAxisLabel: string = 'Population';
+    timeline: boolean = true;
+
     ngOnInit() {
         this.investmentForm.valueChanges.subscribe(formValue => {
             this.computedValue = formValue.initialInvestment * formValue.additionalInvestment;
+            this.data[0].series = this.calculateInvestmentGrowth(formValue);
+            this.data = [...this.data];
         });
+    }
+
+    calculateInvestmentGrowth(formValue: any) {
+        const years = formValue.yearsToGrow;
+        const rate = formValue.expectedRateOfReturn / 100;
+        let investment = formValue.initialInvestment;
+        const additionalInvestment = formValue.additionalInvestment;
+        const frequency = formValue.frequencyOfInvestment === 'Monthly' ? 12 : 1;
+        const growth = [];
+        for (let i = 0; i <= years; i++) {
+            investment = investment + additionalInvestment * frequency;
+            investment = investment * (1 + rate / frequency);
+            growth.push({ name: i, value: investment });
+        }
+        return growth;
     }
 }
