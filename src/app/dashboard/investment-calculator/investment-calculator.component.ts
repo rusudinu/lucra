9651819471b1b4efe86823aaf41b43750e7688f4
emiliaoana <emiliaoana01@gmail.com>
2024-03-06 15:34:55 +0200

    #5 calculator form

--- src/app/dashboard/investment-calculator/investment-calculator.component.ts
+++ src/app/dashboard/investment-calculator/investment-calculator.component.ts
@@ -1,9 +1,9 @@
 import { NgForOf, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatOption } from '@angular/material/autocomplete';
 import { MatButton } from '@angular/material/button';
-import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatError, MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatSelect } from '@angular/material/select';
 import { RouterLink } from '@angular/router';
@@ -11,19 +11,38 @@ import { RouterLink } from '@angular/router';
 @Component({
     selector: 'app-investment-calculator',
     standalone: true,
-    imports: [MatButton, MatError, MatFormField, MatInput, MatLabel, NgIf, ReactiveFormsModule, RouterLink, MatOption, MatSelect, NgForOf],
+    imports: [
+        MatButton,
+        MatError,
+        MatFormField,
+        MatInput,
+        MatLabel,
+        NgIf,
+        ReactiveFormsModule,
+        RouterLink,
+        MatOption,
+        MatSelect,
+        NgForOf,
+        MatPrefix,
+        MatSuffix,
+    ],
     templateUrl: './investment-calculator.component.html',
     styleUrl: './investment-calculator.component.scss',
 })
-export class InvestmentCalculatorComponent {
+export class InvestmentCalculatorComponent implements OnInit {
     investmentForm: FormGroup = new FormGroup({
-        initialInvestment: new FormControl('$', Validators.required),
-        additionalInvestment: new FormControl('', Validators.required),
-        frequencyOfInvestment: new FormControl('', Validators.required),
+        initialInvestment: new FormControl(0, Validators.required),
+        additionalInvestment: new FormControl(0, Validators.required),
+        frequencyOfInvestment: new FormControl('Monthly', Validators.required),
         yearsToGrow: new FormControl('', Validators.required),
         expectedRateOfReturn: new FormControl('', Validators.required),
     });
     frequencyOptions: string[] = ['Monthly', 'Annually'];
+    computedValue: number = 0;
 
-    onCalculateInvestment() {}
+    ngOnInit() {
+        this.investmentForm.valueChanges.subscribe(formValue => {
+            this.computedValue = formValue.initialInvestment * formValue.additionalInvestment;
+        });
+    }
 }
