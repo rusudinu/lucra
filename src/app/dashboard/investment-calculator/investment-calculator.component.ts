cea5a977c9e6ab55a90a7f3a3b99a1b802ee4606
Rusu Dinu <dinu@codingshadows.com>
2024-03-06 21:07:15 +0200

    #5 removed old charts library

--- src/app/dashboard/investment-calculator/investment-calculator.component.ts
+++ src/app/dashboard/investment-calculator/investment-calculator.component.ts
@@ -7,7 +7,6 @@ import { MatError, MatFormField, MatLabel, MatPrefix, MatSuffix } from '@angular
 import { MatInput } from '@angular/material/input';
 import { MatSelect } from '@angular/material/select';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
     selector: 'app-investment-calculator',
@@ -26,7 +25,6 @@ import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
         NgForOf,
         MatPrefix,
         MatSuffix,
-        CanvasJSAngularChartsModule,
         CommonModule,
         RouterOutlet,
     ],
@@ -47,35 +45,6 @@ export class InvestmentCalculatorComponent implements OnInit {
     ngOnInit() {
         this.investmentForm.valueChanges.subscribe(formValue => {
             this.computedValue = formValue.initialInvestment * formValue.additionalInvestment;
-            this.chartOptions.data[0].dataPoints = [
-                { name: 'Start Amount', y: formValue.initialInvestment },
-                { name: 'Total Contribution', y: this.computedValue },
-                { name: 'Interest', y: formValue.expectedRateOfReturn },
-            ];
         });
     }
-
-    chartOptions = {
-        animationEnabled: true,
-        theme: 'white',
-        title: {
-            text: 'Breakdown',
-        },
-        subtitles: [
-            {
-                text: 'Your investment data',
-            },
-        ],
-        data: [
-            {
-                type: 'pie',
-                indexLabel: '{name}: {y}%',
-                dataPoints: [
-                    { name: 'Start Amount', y: 0 },
-                    { name: 'Total Contribution', y: 3.7 },
-                    { name: 'Interest', y: 36.4 },
-                ],
-            },
-        ],
-    };
 }
