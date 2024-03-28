87d2ed179e4c1be4999a73963470e599132426a9
Rusu Dinu <dinu@codingshadows.com>
2024-03-28 13:09:17 +0200

    #2 calculating the total account balance

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+
+import { IAppState } from '../../common/interface/app-state.interface';
+import { selectUser } from '../../user/store/user.selectors';
+import { ERecurrence } from '../transfer/common/recurrence.enum';
+import { ETransactionType, ITransaction } from '../transfer/common/transaction.interface';
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
-    imports: [],
+    imports: [AsyncPipe],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+    store: Store<IAppState> = inject(Store);
+    userData$ = this.store.select(selectUser);
+    totalBalance$ = this.userData$.pipe(
+        map(user =>
+            user.transactions.reduce((acc, transaction: ITransaction) => {
+                if (transaction.recurring) {
+                    let timesOccurred = 0;
+                    const currentDate = new Date();
+                    if (transaction.recurrenceStartDate === undefined) {
+                        return 0;
+                    }
+                    const time = transaction.recurrenceStartDate as any;
+                    const startDate = new Date(time.seconds * 1000 + time.nanoseconds / 1000000);
+
+                    while (startDate < currentDate) {
+                        timesOccurred++;
+                        switch (transaction.recurrencePattern) {
+                            case ERecurrence.Daily:
+                                startDate.setDate(startDate.getDate() + 1);
+                                break;
+                            case ERecurrence.Weekly:
+                                startDate.setDate(startDate.getDate() + 7);
+                                break;
+                            case ERecurrence.Monthly:
+                                startDate.setMonth(startDate.getMonth() + 1);
+                                break;
+                            case ERecurrence.Yearly:
+                                startDate.setFullYear(startDate.getFullYear() + 1);
+                                break;
+                        }
+                    }
+
+                    if (transaction.type === ETransactionType.Income) {
+                        return acc + transaction.amount * timesOccurred;
+                    }
+                    return acc - transaction.amount * timesOccurred;
+                }
+                if (transaction.type === ETransactionType.Income) {
+                    return acc + transaction.amount;
+                }
+                return acc - transaction.amount;
+            }, 0),
+        ),
+    );
+}
